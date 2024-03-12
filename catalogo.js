83b19711eec13b8b827ce7051c13793a885f1576
thiagobueno1 <thiagobueno72@hotmail.com>
2024-03-11 21:23:07 -0300

    primeiro commit

---
+++ catalogo.js
@@ -0,0 +1,76 @@
+let imgArcos = ["https://static.wikia.nocookie.net/naruto/images/2/29/O_Retorno_do_Kazekage.PNG/revision/latest?cb=20130904001442&path-prefix=pt-br",
+"https://criticalhits.com.br/wp-content/uploads/2024/02/Missao-de-Reconhecimento-na-Ponte-Tenchi-1280x893.jpg",
+"https://static.wikia.nocookie.net/naruto/images/8/83/Equipe_de_4_Homens_do_Furido.PNG/revision/latest/smart/width/386/height/259?cb=20160704220806&path-prefix=pt-br",
+"https://lh6.googleusercontent.com/-Gbji7DNm6pc/VNUkre0lpTI/AAAAAAAAAyY/50rAHUIDUz8/w875-h577-no/1.Akatsuki.png",
+"https://static.wikia.nocookie.net/naruto/images/a/a7/Isobu.PNG/revision/latest?cb=20200212142837&path-prefix=pt-br",
+"https://static.wikia.nocookie.net/naruto/images/d/d3/Grupo_Hebi_Reunido.png/revision/latest?cb=20221008015026&path-prefix=pt-br",
+"https://pm1.aminoapps.com/7415/6f993ff8971f737e56c36261cc8846ffa78013f6r1-739-415v2_uhq.jpg",
+"https://criticalhits.com.br/wp-content/uploads/2024/01/Predestinada-Batalha-entre-Irmaos-696x392.jpg",
+"https://static.wikia.nocookie.net/naruto/images/0/0a/Saiken_%28Infobox_-_Parte_II%29.PNG/revision/latest/thumbnail/width/360/height/360?cb=20140619013037&path-prefix=pt-br",
+"https://www.fatosdesconhecidos.com.br/wp-content/uploads/2020/07/Pain_at_Konoha.png",
+"https://static.wikia.nocookie.net/naruto/images/e/e4/Epis%C3%B3dio_192_%28Shippuden%29.PNG/revision/latest/smart/width/250/height/250?cb=20160323185230&path-prefix=pt-br",
+"https://static.wikia.nocookie.net/naruto/images/a/a5/A_Reuni%C3%A3o_dos_Cinco_Kage.png/revision/latest?cb=20131123133147&path-prefix=pt-br",
+"https://static.wikia.nocookie.net/naruto/images/2/22/As_For%C3%A7as_aliadas.PNG/revision/latest?cb=20130711063410&path-prefix=pt-br",
+"https://static.wikia.nocookie.net/naruto/images/b/b3/Naruto_e_sua_equipe_se_preparam_para_partir.png/revision/latest?cb=20141026124559&path-prefix=pt-br",
+"https://static.wikia.nocookie.net/naruto/images/a/aa/Naruto_entra_em_um_modo.png/revision/latest?cb=20140928051112&path-prefix=pt-br",
+"https://criticalhits.com.br/wp-content/uploads/2020/05/naruto-shippuden-quiz-12-perguntas.jpg",
+"https://static.wikia.nocookie.net/naruto/images/e/e0/A_M%C3%A1scara_que_Esconde_o_Cora%C3%A7%C3%A3o.png/revision/latest/scale-to-width-down/1200?cb=20140209210840&path-prefix=pt-br",
+"https://criticalhits.com.br/wp-content/uploads/2018/09/Naruto-Rasengan-users-1280x720.jpg",
+"https://static.wikia.nocookie.net/naruto/images/8/85/Epis%C3%B3dio_413_%28Shippuden%29.png/revision/latest/scale-to-width-down/1200?cb=20180902135646&path-prefix=pt-br",
+"https://static.wikia.nocookie.net/naruto/images/0/00/J%C5%ABbi.PNG/revision/latest?cb=20160415141801&path-prefix=pt-br",
+"https://uploads.spiritfanfiction.com/historias/capas/201810/o-conto-do-galante-jiraya-14695097-271020180836.png",
+"https://1.bp.blogspot.com/-QjimjjP40sY/VsxZolnQjbI/AAAAAAAAlI4/-JtR9RDP4AY/s1600/zgBJWKjBwOo.jpg",
+"https://static.wikia.nocookie.net/fiction-battlefield/images/1/1f/Kaguya-1.png/revision/latest?cb=20200815020031&path-prefix=pt-br",
+"https://static.wikia.nocookie.net/naruto/images/1/1e/Naruto_%28Arco_da_Inf%C3%A2ncia%29.png/revision/latest/scale-to-width-down/1200?cb=20161030194520&path-prefix=pt-br",
+"https://pm1.aminoapps.com/6296/fa42fc6360526d0d70f154dba380b3514c691735_hq.jpg",
+"https://static.wikia.nocookie.net/naruto/images/2/2b/Shikamaru_Shinden_-_Uma_nuvem_dan%C3%A7ando_em_p%C3%A9talas_caindo_abandonadas.png/revision/latest/smart/width/386/height/259?cb=20181026200557&path-prefix=pt-br",
+"https://i.pinimg.com/564x/a7/43/11/a74311e1b7c06f6ca0d872c2e070ceb2.jpg"
+]
+
+let nomeArcos = ["Missão de Resgate do Kazekage","Missão de Reconhecimento na Ponte Tenchi","Doze Guardiões Ninja","Missão de Repressão da Akatsuki","Aparição do Três-Caudas",
+"Missão de Perseguição a Itachi","O Conto de Jiraiya, o Destemido","Predestinada Batalha entre Irmãos","Liberação do Seis-Caudas","Ataque de Pain","Arco do Passado: O Lugar de Konoha",
+"Reunião dos Cinco Kage","Quarta Guerra Mundial Shinobi: Contagem Regressiva","Vida Paradisíaca em um Barco","Poder","Quarta Guerra Mundial Shinobi: Confronto",
+"Quarta Guerra Mundial Shinobi: Confronto","Kakashi Anbu: O Shinobi Que Vive Na Escuridão","Quarta Guerra Mundial Shinobi: Clímax ","Seguindo os Passos de Naruto: Os Caminhos dos Amigos",
+"Nascimento do Jinchūriki do Dez-Caudas","Manual de Instruções de Jiraiya: O Conto de Naruto, o Galante","Livro Itachi Shinden: Luz e Escuridão","Ataque de Kaguya Ōtsutsuk",
+"Arco do Período Juvenil","Sasuke Shinden: Livro da Alvorada","Shikamaru Hiden: Uma Nuvem se Acumulando na Escuridão do Silêncio","Konoha Hiden: O Dia Perfeito para um Casamento"]
+
+let linkArcos = ["https://naruto.fandom.com/pt-br/wiki/Miss%C3%A3o_de_Resgate_do_Kazekage#:~:text=A%20Miss%C3%A3o%20de%20Resgate%20do,Kazekage%20das%20m%C3%A3os%20da%20Akatsuki.",
+"https://naruto.fandom.com/pt-br/wiki/Miss%C3%A3o_de_Reconhecimento_na_Ponte_Tenchi#:~:text=A%20Miss%C3%A3o%20de%20Reconhecimento%20na,da%20parte%20Shipp%C5%ABden%20da%20s%C3%A9rie.",
+"https://naruto.fandom.com/pt-br/wiki/Doze_Guardi%C3%B5es_Ninja",
+"https://naruto.fandom.com/pt-br/wiki/Miss%C3%A3o_de_Repress%C3%A3o_da_Akatsuki#:~:text=A%20Miss%C3%A3o%20de%20Repress%C3%A3o%20da,Hidan%20e%20Kakuzu%20da%20Akatsuki.",
+"https://naruto.fandom.com/pt-br/wiki/Apari%C3%A7%C3%A3o_do_Tr%C3%AAs-Caudas#:~:text=A%20Apari%C3%A7%C3%A3o%20do%20Tr%C3%AAs%2DCaudas,Miss%C3%A3o%20de%20Persegui%C3%A7%C3%A3o%20a%20Itachi.",
+"https://naruto.fandom.com/pt-br/wiki/Miss%C3%A3o_de_Persegui%C3%A7%C3%A3o_a_Itachi#:~:text=A%20Miss%C3%A3o%20de%20Persegui%C3%A7%C3%A3o%20a,Uzumaki%20para%20encontrar%20Itachi%20Uchiha.",
+"https://naruto.fandom.com/pt-br/wiki/O_Conto_de_Jiraiya,_o_Destemido",
+"https://naruto.fandom.com/pt-br/wiki/Predestinada_Batalha_entre_Irm%C3%A3os",
+"https://naruto.fandom.com/pt-br/wiki/Libera%C3%A7%C3%A3o_do_Seis-Caudas#:~:text=A%20Libera%C3%A7%C3%A3o%20do%20Seis%2DCaudas,Caudas%2C%20e%20sua%20aluna%20Hotaru.",
+"https://naruto.fandom.com/pt-br/wiki/Ataque_de_Pain_(Arco)",
+"https://naruto.fandom.com/pt-br/wiki/Arco_do_Passado:_O_Lugar_de_Konoha",
+"https://naruto.fandom.com/pt-br/wiki/Reuni%C3%A3o_dos_Cinco_Kage_(Arco)",
+"https://naruto.fandom.com/pt-br/wiki/Quarta_Guerra_Mundial_Shinobi:_Contagem_Regressiva",
+"https://naruto.fandom.com/pt-br/wiki/Vida_Paradis%C3%ADaca_em_um_Barco",
+"https://naruto.fandom.com/pt-br/wiki/For%C3%A7a_(Arco)",
+"https://naruto.fandom.com/pt-br/wiki/Quarta_Guerra_Mundial_Shinobi:_Confronto",
+"https://naruto.fandom.com/pt-br/wiki/Arco_Kakashi_Anbu:_O_Shinobi_Que_Vive_Na_Escurid%C3%A3o",
+"https://naruto.fandom.com/pt-br/wiki/Quarta_Guerra_Mundial_Shinobi:_Cl%C3%ADmax",
+"https://naruto.fandom.com/pt-br/wiki/Seguindo_os_Passos_de_Naruto:_Os_Caminhos_dos_Amigos",
+"https://naruto.fandom.com/pt-br/wiki/Nascimento_do_Jinchuuriki_do_Dez-Caudas",
+"https://naruto.fandom.com/pt-br/wiki/Manual_de_Instru%C3%A7%C3%B5es_de_Jiraiya:_O_Conto_de_Naruto,_o_Galante",
+"https://naruto.fandom.com/pt-br/wiki/Ataque_de_Kaguya_Ootsutsuki",
+"https://naruto.fandom.com/pt-br/wiki/Arco_do_Per%C3%ADodo_Juvenil#:~:text=O%20Arco%20do%20Per%C3%ADodo%20Juvenil,mem%C3%B3rias%20passadas%20em%20cada%20epis%C3%B3dio.",
+"https://naruto.fandom.com/pt-br/wiki/Sasuke_Shinden:_Livro_da_Alvorada_(Arco)",
+"https://naruto.fandom.com/pt-br/wiki/Shikamaru_Hiden:_Uma_Nuvem_se_Acumulando_na_Escurid%C3%A3o_do_Sil%C3%AAncio_(Arco)",
+"https://naruto.fandom.com/pt-br/wiki/Konoha_Hiden:_O_Dia_Perfeito_para_um_Casamento_(Arco)"]
+
+let container = document.querySelector('.container');
+
+imgArcos.forEach((img, index) => {
+    let item = document.createElement('div');
+    item.classList.add('itens');
+    item.innerHTML = `
+        <a href='${linkArcos[index]}' target='_blank'>
+            <img class='poster' src='${img}' alt='Descrição da Imagem'>
+            <h2 class='nomes'>${nomeArcos[index]}</h2>
+        </a>
+    `;
+    container.appendChild(item);
+});
